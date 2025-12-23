3e84f3f543ea26e54368e91ae089fb97577f16e9
Tam Toucan <dr@tamtoucan.com>
2025-12-23 12:56:09 +0000

    Change case of dll name

--- distancemap/src/cute/cuteDistanceMap.cpp
+++ distancemap/src/cute/cuteDistanceMap.cpp
@@ -1,38 +1,36 @@
-#include <vector>
-#include <cute.h>
 #include "DistanceMapCore.hpp"
 #include "Router.hpp"
+#include <cute.h>
+#include <vector>
 
 // This class is just a wrapper to prove both libraries can talk to each other
 class CuteDistanceMap {
 
 public:
-    CuteDistanceMap() {
-            std::vector< std::vector<int>> grid = {
-                    { '#','#','#','#','#','#','#','#' },
-                    { '#',' ',' ',' ',' ',' ',' ','#' },
-                    { '#','#','#',' ','#','#','#','#' },
-                    { '#','#','#',' ','#','#','#','#' },
-                    { '#',' ','#',' ',' ',' ',' ','#' },
-                    { '#','#','#',' ','#','#','#','#' },
-                    { '#','#','#','#',' ',' ',' ','#' },
-                    { '#','#','#','#','#','#','#','#' }
-            };
-            DistanceMap::Router::Info info;
-            info.mCaveHeight = 8;
-            info.mCellWidth = 8;
-            info.mCellHeight = 8;
-            DistanceMap::DistanceMapCore core;
-            core.initialize(grid, info);
-        }
-    ~CuteDistanceMap() {
-    }
+  CuteDistanceMap() {
+    std::vector<std::vector<int>> grid = {
+        {'#', '#', '#', '#', '#', '#', '#', '#'},
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+        {'#', '#', '#', ' ', '#', '#', '#', '#'},
+        {'#', '#', '#', ' ', '#', '#', '#', '#'},
+        {'#', ' ', '#', ' ', ' ', ' ', ' ', '#'},
+        {'#', '#', '#', ' ', '#', '#', '#', '#'},
+        {'#', '#', '#', '#', ' ', ' ', ' ', '#'},
+        {'#', '#', '#', '#', '#', '#', '#', '#'}};
+    DistanceMap::Router::Info info;
+    info.mCaveHeight = 8;
+    info.mCellWidth = 8;
+    info.mCellHeight = 8;
+    DistanceMap::DistanceMapCore core;
+    core.initialize(grid, info);
+  }
+  ~CuteDistanceMap() {}
 
-    void draw() {
-	    Cute::draw_push_color(Cute::color_white());
-	    Cute::draw_line(Cute::v2(0,0), Cute::v2(100, 100), 2.0f);
-    }
+  void draw() {
+    Cute::draw_push_color(Cute::color_white());
+    Cute::draw_line(cf_v2(0, 0), cf_v2(100, 100), 2.0f);
+  }
 
 private:
-    DistanceMap::DistanceMapCore core;
+  DistanceMap::DistanceMapCore core;
 };
